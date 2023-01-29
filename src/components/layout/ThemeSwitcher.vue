d3f6012553611e54e49f4023d518c21fab619d02
guclusefa <sefaaguclu@gmail.com>
2023-01-29 12:35:20 +0100

    theme a revoir

--- src/components/layout/ThemeSwitcher.vue
+++ src/components/layout/ThemeSwitcher.vue
@@ -1,35 +1,25 @@
 <template>
-    <div>
-      <div class="form-check form-switch">
-        <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckChecked" :checked="isDark" @change="toggleDark()">
-      </div>
-    </div>
-  </template>
-  
-  <script>
-  import { useDark, useToggle } from '@vueuse/core'
-  import { setVar } from 'sass-vars-manager'
-  
-  export default {
-    setup() {
-      const isDark = useDark({
-        selector: "html",
-        attribute: "data-bs-theme",
-        valueDark: "dark",
-        valueLight: "light",
-      });
-  
-      const toggleDark = useToggle(isDark)
-  
-      function changeTheme(theme) {
-        setVar('$theme', theme)
-      }
-  
-      return {
-        isDark,
-        toggleDark,
-        changeTheme
-      }
-    }
-  }
-  </script>
+  <div class="form-check form-switch">
+    <input
+      class="form-check-input"
+      type="checkbox"
+      role="switch"
+      id="flexSwitchCheckChecked"
+      :checked="isDark"
+      @change="toggleDark()"
+    />
+  </div>
+</template>
+
+<script setup>
+import { useDark, useToggle } from "@vueuse/core";
+
+const isDark = useDark({
+  selector: "body", //element to add attribute to
+  attribute: "data-bs-theme", // attribute name
+  valueDark: "dark", // attribute value for dark mode
+  valueLight: "light", // attribute value for light mode
+});
+
+const toggleDark = useToggle(isDark);
+</script>
