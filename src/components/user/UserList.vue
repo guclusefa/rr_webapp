c739cae61ecb122c3d012b8ae38b464911a57c78
guclusefa <sefaaguclu@gmail.com>
2023-02-04 18:11:18 +0100

    filter by gender

--- src/components/user/UserList.vue
+++ src/components/user/UserList.vue
@@ -2,17 +2,46 @@
   <div class="div">
     <div class="col">
       <form @submit.prevent="fetchData({ page: 1 })">
-        <div class="input-group mb-3">
+        <div class="form-group">
+          <label for="search">{{ $t("app.search") }}</label>
           <input
             type="text"
             class="form-control"
-            placeholder="Search"
+            id="search"
             v-model="params.search"
           />
-          <button class="btn btn-primary" type="submit">
-            {{ $t("app.search") }}
-          </button>
         </div>
+
+        <div class="form-group">
+          <label for="certified">{{ $t("users.certified") }}</label>
+          <select
+            class="form-control"
+            id="certified"
+            v-model="params.certified"
+          >
+            <option value=""></option>
+            <option value="1">{{ $t("app.yes") }}</option>
+            <option value="0">{{ $t("app.no") }}</option>
+          </select>
+        </div>
+
+        <div class="form-group">
+          <label for="gender">{{ $t("users.gender") }}</label>
+          <select
+            class="form-select"
+            multiple
+            v-model="params.gender"
+          >
+            <option value=""></option>
+            <option value="M">{{ $t("users.gender.male") }}</option>
+            <option value="F">{{ $t("users.gender.female") }}</option>
+            <option value="O">{{ $t("users.gender.other") }}</option>
+          </select>
+        </div>
+
+        <button class="btn btn-primary" type="submit">
+          {{ $t("app.filter") }}
+        </button>
       </form>
     </div>
   </div>
@@ -26,12 +55,8 @@
       :page="params.page"
       :fetchData="fetchData"
     />
-    <MetaInformation
-      v-if="meta.total > 0"
-      :items="users"
-      :meta="meta"
-    />
-    <NoResults v-if="meta.count === 0" />
+    <MetaInformation v-if="meta.total > 0" :items="users" :meta="meta" />
+    <NoResults v-if="meta.count === 0 && !isLoading" />
   </template>
 
   <NoResponse v-if="isDown" />
@@ -57,6 +82,8 @@ export default {
         limit: 4,
         page: 1,
         search: "",
+        certified: "",
+        gender: [],
       },
       isLoading: false,
       isDown: false,
@@ -74,6 +101,7 @@ export default {
         this.skeletons = Array.from({ length: this.params.limit });
         // merge params with current params
         this.params = { ...this.params, ...params };
+        console.log(this.params);
         // fetch data and set data and meta and check if response
         const response = await fetchData(this.url, this.params);
         !response ? (this.isDown = true) : null;
