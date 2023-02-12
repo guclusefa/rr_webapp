4b8a987f879fab29c88d05a95b56a90072f9b4e7
guclusefa <sefaaguclu@gmail.com>
2023-02-12 12:30:27 +0100

    clean

--- src/services/modules/auth.js
+++ src/services/modules/auth.js
@@ -1,36 +1,19 @@
-import { fetchData, postData } from "@/services/api";
+import { postData } from "@/services/api";
 import { constants, handleMessage } from "@/services/messages";
 import router from "@/router";
 
-export const checkToken = () => {
-    // Check token and set expiration date
-    fetchData("/check-token").then((response) => {
-        // No response
-        if (response == null) return;
-        // Success
-        if (response.status === 200) {
-            localStorage.setItem("expirationDate", response.data.expirationDate);
-        } else {
-            // Error
-            handleMessage(constants.TYPE_ERROR, response.data.errors.message);
-        }
-    });
-};
-
 export const login = (body) => {
     postData("/login", body).then((response) => {
-        // No response
+        // No response or error
         if (response == null) return;
-        // Success
-        if (response.status === 200) {
-            // Set token
-            localStorage.setItem("token", response.data.token);
-            // If all good, redirect to home with success message
-            handleMessage(constants.TYPE_SUCCESS, "login.success", true);
-            router.push({ name: "home" });
-        } else {
-            // Error
+        if (response.status !== 200) {
             handleMessage(constants.TYPE_ERROR, response.data.errors.message);
+            return;
         }
+        // Set token
+        localStorage.setItem("token", response.data.token);
+        // If all good, redirect to home with success message
+        handleMessage(constants.TYPE_SUCCESS, "login.success", true);
+        router.push({ name: "home" });
     })
 };
