44cc62f3226b43380ce3fe1fec7915e289ac0576
guclusefa <sefaaguclu@gmail.com>
2023-03-14 23:06:59 +0100

    hero section

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -11,6 +11,31 @@
         </div>
       </div>
     </section>
+
+    <!-- Hero section -->
+    <section class="mb-5">
+      <div class="row mb-3 border-bottom border-top py-3">
+        <div class="col-12 col-md-6 d-flex align-items-center">
+          <div>
+            <h2>{{ $t("app.name") }}</h2>
+            <p class="lead">
+              {{ $t("home.description") }}
+            </p>
+            <div class="d-flex gap-2">
+              <router-link to="/resources" class="btn btn-primary">
+                {{ $t("resources.page") }}
+              </router-link>
+              <router-link to="/profiles" class="btn btn-outline-primary">
+                {{ $t("profiles.page") }}
+              </router-link>
+            </div>
+          </div>
+        </div>
+        <div class="col-6 d-none d-md-block">
+          <img src="@/assets/images/icons/ico-hero.svg" class="img-fluid" />
+        </div>
+      </div>
+    </section>
   </section>
 </template>
 
